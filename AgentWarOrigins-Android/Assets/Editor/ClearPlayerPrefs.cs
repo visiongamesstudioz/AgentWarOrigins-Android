bb2ea9e265208a5b9cf8ee26eb291a4acc1b4ed9
visiongamesstudioz <visiongamesstudioz@gmail.com>
2018-12-18 12:09:26 +0530

    AWOv1.2.7

---
+++ AgentWarOrigins-Android/Assets/Editor/ClearPlayerPrefs.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class ClearPlayerPrefs : Editor {
+
+    [MenuItem("Utils/Delete All PlayerPrefs")]
+     public static void DeleteAllPlayerPrefs()
+    {
+        PlayerPrefs.DeleteAll();
+        Debug.Log("PlayerPrefs Cleared");
+    }
+}
---
+++ AgentWarOrigins-Android/Assets/Editor/OutfitEditorScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine;
+
+[CustomEditor(typeof(Outfit))]
+public class OutfitEditorScript : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        var outfit = target as Outfit;
+
+        if (outfit)
+        {
+           // OutfitAbility outfitAbility = outfit.OutfitAbility;
+            //if (outfitAbility == Outfit.OutfitAbility.LowDamage)
+            //{
+            //    outfit.DamageMultiplier = EditorGUILayout.FloatField("Damage Multiplier",outfit.DamageMultiplier);
+
+            //}
+
+            //else if (outfitAbility == Outfit.OutfitAbility.Revive)
+            //{
+            //    outfit.NoofReviveTimes = EditorGUILayout.FloatField("No of Revives", outfit.NoofReviveTimes);
+            //    outfit.ReviveEnergyAmountFactor = EditorGUILayout.FloatField("Revive Energy Amount", outfit.ReviveEnergyAmountFactor);
+            //}
+            //EditorGUI.BeginChangeCheck();
+
+            //if (outfit.NoofUpdrages > 0)
+            //{
+            //    SerializedProperty upgradeLockTypes = serializedObject.FindProperty("UpgradeLockTypes");
+            //    for (int i = 0; i < upgradeLockTypes.arraySize; i++)
+            //    {
+            //        Debug.Log("no of enums" + upgradeLockTypes.arraySize );
+            //        SerializedProperty upgradecost = upgradeLockTypes.GetArrayElementAtIndex(i);
+            //        GUIContent upgradecostLabel = new GUIContent(string.Concat("UpgradeLockTypes", i + 1));
+            //        EditorGUILayout.PropertyField(upgradecost, upgradecostLabel);
+            //    }
+            //}
+
+            //EditorGUI.EndChangeCheck();
+        }
+        EditorUtility.SetDirty(target);
+
+
+
+    }
+}
