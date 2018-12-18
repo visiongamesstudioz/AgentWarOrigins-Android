a17f8d97757827c252e5a2ed7a05aba7e4eca11d
visiongamesstudioz <visiongamesstudioz@gmail.com>
2018-12-18 18:42:44 +0530

    AWOv1.2.7

---
+++ AgentWarOrigins-Android/Assets/UI/renders/unity/ScreenShotRender.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenShotRender : MonoBehaviour {
+
+    void Start()
+    {
+        Debug.Log("rendering");
+        ScreenCapture.CaptureScreenshot("Assets/UI/renders/unity/render_2", 2);
+    }
+}
