da8440838056484e0f106aa67836816f01469939
visiongamesstudioz <visiongamesstudioz@gmail.com>
2018-12-18 17:19:01 +0530

    AWOv1.2.7

---
+++ AgentWarOrigins-Android/Assets/Models/Characters/jia/regen_outfit/emissivePulse.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class emissivePulse : MonoBehaviour
+{
+    public float frequency = 1f;
+    public float amplitude = 1f;
+    public float baseMult = 0.75f;
+    public Material material;
+    public Color emissionColor;
+
+    // Use this for initialization
+    void Start()
+    {
+        emissionColor = material.GetColor("_EmissionColor") * baseMult * amplitude;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float glow = (2 + Mathf.Cos(Time.time * frequency)) * amplitude;
+        material.SetColor("_EmissionColor", emissionColor * glow);
+    }
+}
